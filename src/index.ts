2d9603a09d099d10f6646c86391c98922d250525
Syllas <syllas@raiz.dev>
2023-11-04 00:32:31 +0100

    printing async

--- src/index.ts
+++ src/index.ts
@@ -257,38 +257,62 @@ const BLEPrinter = {
       resolve();
     }),
 
-  printText: (text: string, opts: PrinterOptions = {}): void => {
-    if (Platform.OS === "ios") {
-      const processedText = textPreprocessingIOS(text, false, false);
-      RNBLEPrinter.printRawData(
-        processedText.text,
-        processedText.opts,
-        (error: Error) => console.warn(error)
-      );
-    } else {
-      RNBLEPrinter.printRawData(textTo64Buffer(text, opts), (error: Error) =>
-        console.warn(error)
-      );
-    }
+  printText: (text: string, opts: PrinterOptions = {}): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      if (Platform.OS === "ios") {
+        const processedText = textPreprocessingIOS(text, false, false);
+        RNBLEPrinter.printRawData(
+          processedText.text,
+          processedText.opts, // TODO: Success callback for iOS
+          (error: Error) => {
+            console.warn(error);
+            reject(error);
+          }
+        );
+        resolve();
+      } else {
+        RNBLEPrinter.printRawData(
+          textTo64Buffer(text, opts),
+          () => {
+            resolve();
+          },
+          (error: Error) => {
+            console.warn(error);
+            reject(error);
+          }
+        );
+      }
+    });
   },
 
-  printBill: (text: string, opts: PrinterOptions = {}): void => {
-    if (Platform.OS === "ios") {
-      const processedText = textPreprocessingIOS(
-        text,
-        opts?.cut ?? true,
-        opts.beep ?? true
-      );
-      RNBLEPrinter.printRawData(
-        processedText.text,
-        processedText.opts,
-        (error: Error) => console.warn(error)
-      );
-    } else {
-      RNBLEPrinter.printRawData(billTo64Buffer(text, opts), (error: Error) =>
-        console.warn(error)
-      );
-    }
+  printBill: (text: string, opts: PrinterOptions = {}): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      if (Platform.OS === "ios") {
+        const processedText = textPreprocessingIOS(
+          text,
+          opts?.cut ?? true,
+          opts.beep ?? true
+        );
+        RNBLEPrinter.printRawData(
+          processedText.text,
+          processedText.opts, // TODO: Success callback for iOS
+          (error: Error) => {
+            console.warn(error);
+            reject(error);
+          }
+        );
+        resolve();
+      } else {
+        RNBLEPrinter.printRawData(
+          billTo64Buffer(text, opts),
+          () => resolve(),
+          (error: Error) => {
+            console.warn(error);
+            reject(error);
+          }
+        );
+      }
+    });
   },
   /**
    * image url
@@ -341,12 +365,21 @@ const BLEPrinter = {
    * android print with encoder
    * @param text
    */
-  printRaw: (text: string): void => {
-    if (Platform.OS === "ios") {
-    } else {
-      RNBLEPrinter.printRawData(text, (error: Error) => console.warn(error));
-    }
-  },
+  printRaw: (text: string): Promise<void> =>
+    new Promise((resolve, reject) => {
+      if (Platform.OS === "ios") {
+        reject("Not supported");
+      } else {
+        RNBLEPrinter.printRawData(
+          text,
+          () => resolve(),
+          (error: Error) => {
+            console.warn(error);
+            reject(error);
+          }
+        );
+      }
+    }),
   /**
    * `columnWidth`
    * 80mm => 46 character
@@ -358,26 +391,36 @@ const BLEPrinter = {
     columnAlignment: ColumnAlignment[],
     columnStyle: string[],
     opts: PrinterOptions = {}
-  ): void => {
-    const result = processColumnText(
-      texts,
-      columnWidth,
-      columnAlignment,
-      columnStyle
-    );
-    if (Platform.OS === "ios") {
-      const processedText = textPreprocessingIOS(result, false, false);
-      RNBLEPrinter.printRawData(
-        processedText.text,
-        processedText.opts,
-        (error: Error) => console.warn(error)
+  ): Promise<void> =>
+    new Promise((resolve, reject) => {
+      const result = processColumnText(
+        texts,
+        columnWidth,
+        columnAlignment,
+        columnStyle
       );
-    } else {
-      RNBLEPrinter.printRawData(textTo64Buffer(result, opts), (error: Error) =>
-        console.warn(error)
-      );
-    }
-  },
+      if (Platform.OS === "ios") {
+        const processedText = textPreprocessingIOS(result, false, false);
+        RNBLEPrinter.printRawData(
+          processedText.text,
+          processedText.opts,
+          (error: Error) => {
+            console.warn(error);
+            reject(error);
+          }
+        );
+        resolve();
+      } else {
+        RNBLEPrinter.printRawData(
+          textTo64Buffer(result, opts),
+          () => resolve(),
+          (error: Error) => {
+            console.warn(error);
+            reject(error);
+          }
+        );
+      }
+    }),
 };
 
 const NetPrinter = {
